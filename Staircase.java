1644fbad11dd53ea5c0f7311d2bdec93985915dc
Sumitesh Naithani <sumiteshn@gmail.com>
2019-05-19 09:46:46 +0530

    Create Staircase.java

---
+++ Staircase.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        char grid[][]=new char[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if((i+j)<n-1)
+                grid[i][j]=' ';
+                else
+                grid[i][j]='#';
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
